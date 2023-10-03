a72a72a456c06980ff9dc2c19b2020d22b5ac7a7
Rafael Rosa <rafagrosa@gmail.com>
2023-10-03 09:48:46 -0300

    alterações e correções de bugs

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
+let tasksLeft = 0;
+
 function addTask() {
     const taskInput = document.getElementById("taskInput");
     const taskText = taskInput.value.trim();
 
     if (taskText !== "") {
         const taskList = document.getElementById("taskList");
-
         const listItem = document.createElement("li");
-        listItem.innerHTML = `${taskText}<li onclick="toggleStrikethrough(this)"></li>`;
+        tasksLeft++;
+
+        listItem.addEventListener("click", function() {
+            toggleStrikethrough(listItem);
+        }, false);
+        
+        listItem.innerHTML = `${taskText}`;
 
         taskList.appendChild(listItem);
         taskInput.value="";
@@ -15,14 +22,16 @@ function addTask() {
     
 }
 
-
 function toggleStrikethrough(element) {
+    
     if (element.style.textDecoration === "line-through") {
         element.style.textDecoration = "none";
     } else {
         element.style.textDecoration = "line-through";
+        tasksLeft--;
     }
-    $("#taskList").after(alert("Parabéns, você terminou suas tarefas!"));
+
+    if (tasksLeft === 0) {
+        $("#taskList").after(alert("Parabéns, você terminou suas tarefas!"));
+    }
 }
-
-
